4e27038249ac3f0e4348c2abbf4fe7b97f3db7ba
Vitalii Kozmyn <vitaliikozmyn@gmail.com>
2025-03-10 19:56:35 +0200

    + add 1.2.10

--- lab2/js/script.js
+++ lab2/js/script.js
@@ -17,7 +17,7 @@ car1.numberOfAccidents = 0;
 let car2 = {
     color: "scarlet",
     maxSpeed: 120,
-    diver: {
+    driver: {
         name: "Vitalii Kozmyn",
         category: "B",
         personalLimitations: null,
@@ -41,5 +41,55 @@ car1.drive = function() {
 //...........................................................
 
 //1.2.6......................................................
-car2.drive();
+// car2.drive();
+//...........................................................
+
+//1.2.7......................................................
+function Truck(color, weight, avgSpeed, brand, model) {
+    this.color = color;
+    this.weight = weight;
+    this.avgSpeed = avgSpeed;
+    this.brand = brand;
+    this.model = model;
+    this.driver = null;
+}
+//...........................................................
+
+//1.2.8......................................................
+Truck.prototype.AssignDriver = function(name, nightDriving, experience) {
+    this.driver = {
+        name: name,
+        nightDriving: nightDriving,
+        experience: experience,
+    }
+}
+//...........................................................
+
+//1.2.9......................................................
+Truck.prototype.trip = function () {
+    if(this.driver) {
+        let checknightDriving;
+        if(this.driver.nightDriving == true) {
+            checknightDriving = " drives at night";
+        } else {
+            checknightDriving = " does not drive at night";
+        }
+        console.log(`Driver ${this.driver.name} ${checknightDriving} and has ${this.driver.experience} years of experience.`);
+    } else {
+        console.log("No driver assigned");
+    }
+}
+//...........................................................
+
+//1.2.10......................................................
+// let carTruck1 = new Truck("Red", 5000, 80, "Ford", "Fiesta");
+// carTruck1.trip()//No driver assigned
+
+// let carTruck2 = new Truck("Green", 4500, 85, "Ford", "Edge");
+// carTruck2.AssignDriver("Vitalii Kozmyn", true, 5);
+// carTruck2.trip(); //drives at night
+
+// let carTruck3 = new Truck("Green", 6500, 60, "Ford", "F-150 Lightning");
+// carTruck3.AssignDriver("Vitalii Kozmyn", false, 5);
+// carTruck3.trip(); //does not drive at night
 //...........................................................
